183a4677e61bc5c1dded7de86a86abb945258310
Arthur Papailhau <papailhau@uber.com>
2020-04-18 00:03:03 -0700

    Updates icon Lifegorithms

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,7 +173,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#1184e8`,
         display: `minimal-ui`,
-        // icon: `src/assets/icon.png`,
+        icon: `src/assets/icon.png`,
         theme_color_in_head: false,
       },
     },
